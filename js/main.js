2d307202c3f994aaea10885ca402380724c474f7
user.email <abdoulraoufharouna62@gmail.com>
2025-10-24 08:26:00 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+      AOS.init({
+            duration:800,
+            easing:'ease-in-out',
+            once:false,
+            mirror:true,
+            offset:100
+      })
+})
