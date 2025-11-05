bac7685e91ae2dfa30f5407efc23d497dbd4d113
user.email <abdoulraoufharouna62@gmail.com>
2025-11-05 03:48:52 +0100

    c'est parfait

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+window.addEventListener("load", () => {
+      const container = document.getElementById("Zone_Image")
+
+      const img = document.createElement("img")
+      img.src = "asset/Hero Man/Ma_Photo2.png"
+      img.alt = "Ma_Photo"
+      img.classList.add("img-fluid", "w-auto")
+      container.appenChild(img)
+})
+
+document.addEventListener('DOMContentLoaded', () => {
       AOS.init({
-            duration:800,
-            easing:'ease-in-out',
-            once:false,
-            mirror:true,
-            offset:100
+            duration: 800,
+            easing: 'ease-in-out',
+            once: false,
+            mirror: true,
+            offset: 100
       })
 })
